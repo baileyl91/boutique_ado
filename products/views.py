3da094306f6b9a5204a6884cc1d4c6b8908cf9ef
Luke Bailey <baileyl1991@gmail.com>
2022-03-08 18:02:16 +0000

    started products templates

--- products/views.py
+++ products/views.py
@@ -6,7 +6,7 @@ from .models import Product
 def all_products(request):
     """ A view to show all products, including sortng and search queries """
 
-    products = Products.objects.all()
+    products = Product.objects.all()
 
     context = {
         'products': products,
